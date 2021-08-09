2fa4a416a760fc9c1da549edd2bccaadd2d66688
nkopak <123nazik@gmail.com>
2021-08-10 01:12:57 +0300

    localStorage fix

--- services/authService.ts
+++ services/authService.ts
@@ -7,23 +7,13 @@ const logout = async (): Promise<void> => {
   await localStorage.removeItem('token');
 };
 
-// function handleResponse(response: any) {
-//   return response.text().then((text: string) => {
-//     const data = text && JSON.parse(text);
-//     if (!response.ok) {
-//       if (response.status === 401) {
-//         // auto logout if 401 response returned from api
-//         logout();
-//         window.location.reload(true);
-//       }
-
-//       const error = (data && data.message) || response.statusText;
-//       return Promise.reject(error);
-//     }
-
-//     return data;
-//   });
-// }
+const checkLoginStatus = () => {
+  let status;
+  if (typeof window !== 'undefined') {
+    status = localStorage.getItem('token');
+  }
+  return status;
+};
 
 const login = async (
   email: string,
@@ -53,8 +43,10 @@ const register = async (userData: User): Promise<any> => {
 
   return response;
 };
+
 export default {
   login,
   logout,
-  register
+  register,
+  checkLoginStatus
 };

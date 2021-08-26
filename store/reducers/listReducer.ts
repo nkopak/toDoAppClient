3f91ce718149b8adb39b3e572484de78bbe8fc84
nkopak <123nazik@gmail.com>
2021-08-26 18:33:18 +0300

    sorting function

--- store/reducers/listReducer.ts
+++ store/reducers/listReducer.ts
@@ -7,7 +7,8 @@ import {
   LIST_DELETE_LIST,
   listState,
   listAction,
-  LIST_UPDATE_LIST
+  LIST_UPDATE_LIST,
+  IList
 } from '../../types/list';
 
 const initialState: listState = {
@@ -16,38 +17,31 @@ const initialState: listState = {
   error: ''
 };
 
+const arraySortingByListTitle = (arr: Array<IList>) => {
+  arr.sort((a: IList, b: IList) => {
+    const titleA = a.todoTitle.toUpperCase();
+    const titleB = b.todoTitle.toUpperCase();
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+
+    return 0;
+  });
+};
+
 const listReducer = (state = initialState, action: listAction) => {
   switch (action.type) {
     case LIST_SET_LISTS:
-      action.payload.sort((a, b) => {
-        const titleA = a.todoTitle.toUpperCase();
-        const titleB = b.todoTitle.toUpperCase();
-        if (titleA < titleB) {
-          return -1;
-        }
-        if (titleA > titleB) {
-          return 1;
-        }
-
-        return 0;
-      });
+      arraySortingByListTitle(action.payload);
       return { ...state, lists: action.payload };
 
     case LIST_ADD_LIST: {
       const { lists } = state;
       lists.push(action.payload);
-      lists.sort((a, b) => {
-        const titleA = a.todoTitle.toUpperCase();
-        const titleB = b.todoTitle.toUpperCase();
-        if (titleA < titleB) {
-          return -1;
-        }
-        if (titleA > titleB) {
-          return 1;
-        }
-
-        return 0;
-      });
+      arraySortingByListTitle(lists);
 
       return { ...state, lists };
     }
